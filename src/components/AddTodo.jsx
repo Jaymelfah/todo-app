9ec5bb65980c4d6aad3ccf7da8d1f0dd786953d6
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-04-06 05:43:11 +0000

    Setup input form and todo container

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsPlusLg } from 'react-icons/bs';
+import { nanoid } from 'nanoid';
 
-const AddTodo = () => (
-  <div>
-    <h1>AddTodoContainer</h1>
-  </div>
-);
+const AddTodo = () => {
+  const [item, setItem] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const todo = {
+      id: nanoid(),
+      task: item,
+    };
+    console.log(todo);
+  };
+
+  return (
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="input"
+          name="item"
+          value={item}
+          type="text"
+          placeholder="Add todo item"
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button type="submit">
+          <BsPlusLg />
+        </button>
+      </form>
+    </div>
+  );
+};
 export default AddTodo;
--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { BsTrash } from 'react-icons/bs';
 
-const TodoContainer = () => (
-  <div>
-    <h1>TodoContainer</h1>
-  </div>
-);
+const TodoContainer = () => {
+  const todoList = [
+    {
+      id: 1,
+      task: 'eat grocery',
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      task: 'eat grocery',
+      isCompleted: false,
+    },
 
+    {
+      id: 3,
+      task: 'eat grocery',
+      isCompleted: false,
+    },
+
+  ];
+
+  const updateTodo = () => {
+    // const updatedTodoList = todoList.map((item) => {
+    //   if (item.id === id) {
+    //     return {
+    //       ...item,
+    //       isCompleted: !item.isCompleted,
+    //     };
+    //   }
+    //   return item;
+    // });
+    // dispatch(updateTodoList(updatedTodoList));
+  };
+
+  return (
+    <div>
+      {
+      todoList.map((item) => (
+        <div key={item.id}>
+          <input
+            type="checkbox"
+            checked={item.isCompleted}
+            onChange={() => updateTodo(item.id)}
+          />
+          <span>{item.task}</span>
+          <BsTrash />
+        </div>
+      ))
+    }
+    </div>
+  );
+};
 export default TodoContainer;
